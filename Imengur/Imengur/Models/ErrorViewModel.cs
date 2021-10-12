f9f76d9a6d823d86b96c579a9cba5e7a5c55fe39
Bąk Jan <jan.bak@microsoft.wsei.edu.pl>
2021-10-12 13:44:50 +0200

    first commit

---
+++ Imengur/Imengur/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Imengur.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
